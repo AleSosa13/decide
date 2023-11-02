f341d07cde3f135f183a75a8990539bb26afef06
Alex SC <alesoscor@alum.us.es>
2023-11-02 16:56:11 +0100

    Various Docker files modifications

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD':'',
+        'PASSWORD':'postgrespass123',
         'HOST': 'db',
         'PORT': 5432,
     }
